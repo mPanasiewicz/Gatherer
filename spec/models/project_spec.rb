bef4ab18d8eb4c06c7aeaa3f73acba8939091f9b
Maciej Panasiewicz <maciej.panasiewicz@mfind.pl>
2015-12-13 22:26:35 +0100

    Progress of completed/remaining tasks.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,5 +14,32 @@ RSpec.describe Project do
       expect(project).not_to be_done
     end
 
+    it "marks a project done if its tasks are done" do
+      project.tasks << task
+      task.mark_completed
+      expect(project).to be_done
+    end
+
   end
+
+  describe "estimates" do
+    let(:project) { Project.new }
+    let(:done) { Task.new(size: 2, completed: true) }
+    let(:large_not_done) { Task.new(size: 1) }
+    let(:small_not_done) { Task.new(size: 4) }
+
+    before(:example) do
+      project.tasks = [done, small_not_done, large_not_done]
+    end
+
+    it "can calculate total size" do
+      expect(project.total_size).to eq(7)
+    end
+
+    it "can calculate remaining size" do
+      expect(project.remaining_size).to eq(5)
+    end
+
+  end
+
 end
